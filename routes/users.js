3aa49ae00e94ddb1484faf39104ea59f7313bc40
Rutuja <rutujadshende@gmail.com>
2020-10-03 11:35:19 +0530

    mongoose population

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,6 @@ var User = require('../Models/user');
 var passport = require('passport');
 var authenticate = require('../authenticate');
 
-
 var router = express.Router();
 router.use(bodyParser.json());
 
@@ -13,19 +12,31 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/signup',function(req, res, next){
-  User.register(new User({username: req.body.username}),
-  req.body.password, (err, user) => {
+router.post('/signup',(req, res, next) => {
+  User.register(new User({username: req.body.username}), 
+    req.body.password, (err, user) => {
     if(err) {
       res.statusCode = 500;
       res.setHeader('Content-Type', 'application/json');
       res.json({err: err});
     }
     else {
-      passport.authenticate('local')(req, res, () => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({success: true, status: 'Registration Successful!'});
+      if (req.body.firstname)
+        user.firstname = req.body.firstname;
+      if (req.body.lastname)
+        user.lastname = req.body.lastname;
+      user.save((err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+          return ;
+        }
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({success: true, status: 'Registration Successful!'});
+        });
       });
     }
   });
