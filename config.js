808b1249bc92063a552455bc9fb4f98869bdfbae
Rutuja <rutujadshende@gmail.com>
2020-10-24 17:10:38 +0530

    passport-facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12347-56789-86380-45632',
-    'mongoUrl' : 'mongodb://localhost:27017/confu'
+    'mongoUrl' : 'mongodb://localhost:27017/confu',
+    'facebook' : {
+        clientId : '3394313727316863',
+        clientSecret : 'a05c7d4b685d92134b9814f5a479f682'
+    }
 }
